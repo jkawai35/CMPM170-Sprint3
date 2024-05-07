436430ab5a2bc5818f06a8a9b7b8239b833f78fe
scso-ucsc <scso@ucsc.edu>
2024-05-06 21:28:33 -0700

    Finalising Movement and Jump Inputs

--- Assets/Managers/PlayerManager.cs
+++ Assets/Managers/PlayerManager.cs
@@ -2,43 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviour //Script is based on Code Class - 2D Player Movement in Unity: https://www.youtube.com/watch?v=0-c3ErDzrh8
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float playerSpeed, playerDrag;
-    private float horizontalMovement, verticalMovement;
+    [SerializeField] private float playerMoveSpeed, playerJumpSpeed, playerAcceleration;
+    [Range(0f, 1f)] [SerializeField] private float playerDrag;
+    [SerializeField] private BoxCollider2D groundCheckCollider;
+    [SerializeField] private LayerMask groundMask;
+    private float horizontalMovement, moveIncrement, totalHorizontalSpeed;
     private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerSpeed = 5f;
-        verticalMovement = 0f;
+        playerMoveSpeed = 5f;
+        playerJumpSpeed = 7.5f;
+        playerAcceleration = 1f;
+        playerDrag = 0.9f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMovement = Input.GetAxis("Horizontal");
-        verticalMovement = Input.GetAxis("Vertical");
-
-        if(Mathf.Abs(horizontalMovement) > 0){
-            rb.velocity = new Vector2(horizontalMovement * playerSpeed, rb.velocity.y);
-        }
-
-        if(Mathf.Abs(verticalMovement) > 0){
-            rb.velocity = new Vector2(rb.velocity.x, verticalMovement * playerSpeed);
+        getPlayerInput();
+        if(Input.GetKey(KeyCode.J) && isGrounded){ //Player will jump if J is down
+            playerJump();
         }
     }
 
+    //Fixed Update is called at specific rates defined by the Unity editor
     void FixedUpdate(){
         checkIfGrounded();
-        if(isGrounded){
-            rb.velocity *= playerDrag;
+        implementFriction();
+        movePlayer();
+    }
+
+    private void getPlayerInput(){ //Adjusts horizontal movement force depending on player input
+        if(Input.GetKey(KeyCode.R)){ //Player will move Right when R is pressed
+            horizontalMovement = 1f;
+        } else if(Input.GetKey(KeyCode.L)){ //Player will move Left when L is pressed
+            horizontalMovement = -1f;
+        } else{
+            horizontalMovement = 0f;
         }
     }
 
-    private void checkIfGrounded(){
+    private void movePlayer(){
+        if(horizontalMovement != 0){ //Apply movement if movement force != 0
+            moveIncrement = horizontalMovement * playerAcceleration; //Applying acceleration if movement key is still being pressed
+            totalHorizontalSpeed = Mathf.Clamp(rb.velocity.x + moveIncrement, -playerMoveSpeed, playerMoveSpeed);
 
+            rb.velocity = new Vector2(totalHorizontalSpeed, rb.velocity.y);
+        } else{ //Stop movement if no correct movement key is being pressed
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+    }
+
+    private void playerJump(){ //Adding y-force to player
+        rb.velocity = new Vector2(rb.velocity.x, playerJumpSpeed);
+    }
+
+    private void checkIfGrounded(){ //If ground collider is overlapping objects in Ground Layer, make is Grounded true
+        isGrounded = Physics2D.OverlapAreaAll(groundCheckCollider.bounds.min, groundCheckCollider.bounds.max, groundMask).Length > 0;
+    }
+
+    private void implementFriction(){ //Adding Friction to player if touching ground and no movement is being inputed
+        if(isGrounded && horizontalMovement == 0 && rb.velocity.y <= 0){
+            rb.velocity *= playerDrag;
+        }
     }
 }
