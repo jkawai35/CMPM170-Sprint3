f5957994ed1446f844a926a1909e5cdf3b2ee3f9
scso-ucsc <scso@ucsc.edu>
2024-05-10 13:31:38 -0700

    Writing PlayerProjectile Script

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Update(){
+        if(this.gameObject.transform.position.x >= 10 || this.gameObject.transform.position.x <= -10){ //If fireball goes off screen, deactivate and reset velocity to 0
+            this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            this.gameObject.SetActive(false);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D collider){ //If fireball hits enemy or ground, deactivate it
+        this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        if(collider.gameObject.layer == 7){ //If Layer == "Ground"
+            this.gameObject.SetActive(false);
+        } 
+        // else if(collider.gameObject.layer == "Enemy"){
+        //     this.gameObject.SetActive(false);
+        // }
     }
 }
