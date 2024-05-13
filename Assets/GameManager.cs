4ec26ea01bdc27d281f433b1882e29e09a188219
scso-ucsc <scso@ucsc.edu>
2024-05-12 21:19:59 -0700

    Creating GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private bool isGameOver;
+
+    void Awake(){
+        if(instance == null){
+            instance = this;
+        } else{
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool getIsGameOver(){
+        return isGameOver;
+    }
+
+    public void gameOver(){
+        isGameOver = true;
+    }
+}
