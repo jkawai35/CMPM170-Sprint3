01a01fa3f0780b3e1c57d224dd75c0ddd5ef691e
Cal Friedman School <cffriedm@ucsc.edu>
2024-05-17 11:30:57 -0700

    cannon fix

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,7 +12,7 @@ public class Projectile : MonoBehaviour
         startPos = this.gameObject.transform.position.x;
     }
     void Update(){
-        rb.velocity = transform.right*speed;
+        rb.velocity = transform.right*speed*-1;
         if(this.gameObject.transform.position.x >= startPos+30 || this.gameObject.transform.position.x <= startPos-30){ //If fireball goes too far away from the cannon, deactivate and reset velocity to 0
             this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
             this.gameObject.SetActive(false);
@@ -23,9 +23,9 @@ public class Projectile : MonoBehaviour
         if(collider.gameObject.layer == 7){ //If Layer == "Ground"
             Debug.Log("cannon projectile hit ground");
             gameObject.SetActive(false);
-        } else if(collider.gameObject.layer == 6){
+        } /*else if(collider.gameObject.layer == 6){
             gameObject.SetActive(false);
-        }
+        }*/
     }
 
 }
