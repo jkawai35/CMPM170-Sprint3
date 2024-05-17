5549dcb51541198c2ad8d8ecc9dc810214f2d64a
scso-ucsc <scso@ucsc.edu>
2024-05-16 18:33:33 -0700

    Updating Fireball and Enemy Interaction

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
-    void Update(){
-        // if(this.gameObject.transform.position.x >= 10 || this.gameObject.transform.position.x <= -10){ //If fireball goes off screen, deactivate and reset velocity to 0
-        //     this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-        //     this.gameObject.SetActive(false);
-        // }
-    }
     private void OnCollisionEnter2D(Collision2D collider){ //If fireball hits enemy or ground, deactivate it
         this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         if(collider.gameObject.layer == 7){ //If Layer == "Ground"
             this.gameObject.SetActive(false);
-            Debug.Log("Collision Detected");
-        } 
-        // else if(collider.gameObject.layer == "Enemy"){
-        //     this.gameObject.SetActive(false);
-        // }
+        } else if(collider.gameObject.layer == 9){ //If Layer == "Enemies"
+            this.gameObject.SetActive(false);
+        }
     }
 }
